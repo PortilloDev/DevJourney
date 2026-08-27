72b175cb6f5e9494ae7db612904417a4a4da41f0
ivan <ivan.portillo@mo2o.com>
2026-08-27 11:51:13 +0200

    config traffic with proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'track/*',
         ]);
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
